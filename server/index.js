d2136140e0c69147e814a178cc15572d025ea47a
vaishnavi <dhumalv40@gmail.com>
2026-02-12 12:07:01 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -17,79 +17,88 @@ const io = new Server(server, {
 
 const videos = [
   {
-    _id: "1",
-    videotitle: "Wildlife Safari Adventure",
+    _id: "101",
+    videotitle: "Space Exploration Documentary",
     thumbnail:
-      "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
-    channel: "Discovery World",
-    views: 55000,
+      "https://images.unsplash.com/photo-1446776811953-b23d57bd21aa",
+    channel: "Space Hub",
+    views: 180000,
     videoUrl:
-      "https://media.w3.org/2010/05/bunny/trailer.mp4",
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm",
   },
   {
-    _id: "2",
-    videotitle: "JavaScript Full Course 2026",
+    _id: "102",
+    videotitle: "Node.js Crash Course",
     thumbnail:
-      "https://images.unsplash.com/photo-1555066931-4365d14bab8c",
-    channel: "Code Master",
-    views: 210000,
+      "https://images.unsplash.com/photo-1516116216624-53e697fedbea",
+    channel: "Backend Dev",
+    views: 95000,
+    videoUrl:
+      "https://media.w3.org/2010/05/video/movie_300.mp4",
+  },
+  {
+    _id: "103",
+    videotitle: "Mountain Travel Vlog",
+    thumbnail:
+      "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+    channel: "Travel Diaries",
+    views: 67000,
     videoUrl:
       "https://media.w3.org/2010/05/sintel/trailer.mp4",
   },
   {
-    _id: "3",
-    videotitle: "Future Technology & AI",
+    _id: "104",
+    videotitle: "Street Food Bangkok",
     thumbnail:
-      "https://images.unsplash.com/photo-1518779578993-ec3579fee39f",
-    channel: "Tech Vision",
-    views: 99000,
+      "https://images.unsplash.com/photo-1526318472351-bc6c2b6b54f4",
+    channel: "Food Street",
+    views: 112000,
     videoUrl:
-      "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+      "https://media.w3.org/2010/05/bunny/movie.mp4",
   },
   {
-    _id: "4",
-    videotitle: "Street Food India Tour",
+    _id: "105",
+    videotitle: "AI & Machine Learning Explained",
     thumbnail:
-      "https://images.unsplash.com/photo-1504674900247-0877df9cc836",
-    channel: "Food Explorer",
-    views: 87000,
+      "https://images.unsplash.com/photo-1677442136019-21780ecad995",
+    channel: "AI Talks",
+    views: 143000,
+    videoUrl:
+      "https://media.w3.org/2010/05/bunny/trailer.mp4",
+  },
+  {
+    _id: "106",
+    videotitle: "React Project Tutorial",
+    thumbnail:
+      "https://images.unsplash.com/photo-1633356122544-f134324a6cee",
+    channel: "React School",
+    views: 88000,
     videoUrl:
       "https://media.w3.org/2010/05/video/movie_300.mp4",
   },
-
-   {
-    _id: "5",
-    videotitle: "Amazing Nature Documentary",
-    thumbnail: "https://i.ytimg.com/vi/6lt2JfJdGSY/maxresdefault.jpg",
-    channel: "Nature Channel",
-    views: 45000,
-    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+  {
+    _id: "107",
+    videotitle: "Rainforest Nature Sounds",
+    thumbnail:
+      "https://images.unsplash.com/photo-1502082553048-f009c37129b9",
+    channel: "Nature Relax",
+    views: 56000,
+    videoUrl:
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm",
   },
   {
-    _id: "6",
-    videotitle: "React Full Course for Beginners",
-    thumbnail: "https://i.ytimg.com/vi/bMknfKXIFA8/maxresdefault.jpg",
-    channel: "Code Academy",
-    views: 120000,
-    videoUrl: "https://www.w3schools.com/html/movie.mp4",
-  },
-  {
-    _id: "7",
-    videotitle: "Top Tech Gadgets 2025",
-    thumbnail: "https://i.ytimg.com/vi/GV3HUDMQ-F8/maxresdefault.jpg",
-    channel: "Tech World",
-    views: 78000,
-    videoUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
-  },
-  {
-    _id: "8",
-    videotitle: "Street Food Around the World",
-    thumbnail: "https://i.ytimg.com/vi/5qap5aO4i9A/maxresdefault.jpg",
-    channel: "Food Vlogs",
+    _id: "108",
+    videotitle: "Top Programming Tips 2026",
+    thumbnail:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4",
+    channel: "Dev Shorts",
     views: 99000,
-    videoUrl: "https://media.w3.org/2010/05/sintel/trailer.mp4",
+    videoUrl:
+      "https://media.w3.org/2010/05/sintel/trailer.mp4",
   },
 ];
+
+
 
 app.get("/video/getall", (req, res) => {
   res.json(videos);
